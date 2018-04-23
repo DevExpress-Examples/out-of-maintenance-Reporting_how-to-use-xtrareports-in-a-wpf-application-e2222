5720c11abaf730aa86c0f736d533853a661a1a6a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MinimalisticReportPreviewDemo/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Themes;
+using DevExpress.Xpf.Printing;
+// ...
+
+namespace MinimalisticReportPreviewDemo {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            ThemeManager.ApplicationThemeName = "Azure";
+            InitializeComponent();
+        }
+
+        private void ShowPreview(object sender, RoutedEventArgs e) {
+            Report report = new Report();
+            XtraReportPreviewModel model = new XtraReportPreviewModel(report);
+            DocumentPreviewWindow window = new DocumentPreviewWindow() { Model = model };
+            report.CreateDocument(true);
+            window.Show();
+        }
+
+    }
+}
