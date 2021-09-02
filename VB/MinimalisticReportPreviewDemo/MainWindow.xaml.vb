35d4b60f747ef563ead4d89a69deec2bfdf8cc71
DevExpressExampleBot <fakeEmail>
2021-09-02 19:46:16 +0300

    autogenerate VB

--- VB/MinimalisticReportPreviewDemo/MainWindow.xaml.vb
+++ VB/MinimalisticReportPreviewDemo/MainWindow.xaml.vb
@@ -4,24 +4,24 @@ Imports DevExpress.Xpf.Printing
 ' ...
 
 Namespace MinimalisticReportPreviewDemo
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
-    Partial Public Class MainWindow
-        Inherits Window
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            ApplicationThemeHelper.ApplicationThemeName = "Office2007Silver"
-            InitializeComponent()
-        End Sub
+		Public Sub New()
+			ApplicationThemeHelper.ApplicationThemeName = "Office2007Silver"
+			InitializeComponent()
+		End Sub
 
-        Private Sub ShowPreview(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            Dim report As New Report()
-            Dim window As New DocumentPreviewWindow()
-            window.PreviewControl.DocumentSource = report
-            report.CreateDocument(True)
-            window.ShowDialog()
-        End Sub
+		Private Sub ShowPreview(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim report As New Report()
+			Dim window As New DocumentPreviewWindow()
+			window.PreviewControl.DocumentSource = report
+			report.CreateDocument(True)
+			window.ShowDialog()
+		End Sub
 
-    End Class
+	End Class
 End Namespace
